8ccd1e67deb853c393d079382fc1cf7c22d9c174
lestariayu99 <lestariayu0801@gmail.com>
2023-06-03 23:14:43 +0700

    1. mengenal komponen

--- src/Materi/Komponen/index.jsx
+++ src/Materi/Komponen/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ClassComponent from "./Pembahasan/ClassComponent";
+import FunctionalComponent from "./Pembahasan/FunctionalComponent";
 
 export default class Komponen extends React.Component {
 
     render() {
         return (
             <div>
-                <ClassComponent />
+                <ClassComponent nama= "Isabella"/>
+                <FunctionalComponent nama= {23} />
             </div>
         )
     }
