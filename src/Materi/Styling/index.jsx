1344dfec88e7a6655bcf863498e63388645ed39b
lestariayu99 <lestariayu0801@gmail.com>
2023-06-04 00:43:18 +0700

    modified folder

---
+++ src/Materi/Styling/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sass from "./Sass ";
+
+export default class Styling extends React.Component {
+    render() {
+        return (
+            <div>
+                <Sass />
+            </div>
+        )
+    }
+}
