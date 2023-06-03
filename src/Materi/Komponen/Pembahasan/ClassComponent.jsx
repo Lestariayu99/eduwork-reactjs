ee2d888348efff510494e114220bb7e5a6a8b7dd
lestariayu99 <lestariayu0801@gmail.com>
2023-06-03 11:12:17 +0700

    blackbox

---
+++ src/Materi/Komponen/Pembahasan/ClassComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Komponen ini dibuat dengan class Component</h1>
+                <h2>Hello duniaku</h2>
+            </div>
+        )
+    }
+}
+
+export default ClassComponent;
