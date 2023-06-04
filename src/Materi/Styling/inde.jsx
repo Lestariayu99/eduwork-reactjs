b8ec079c2ffd6638f0316c95aec825e879d7410a
lestariayu99 <lestariayu0801@gmail.com>
2023-06-04 22:15:43 +0700

    modified src

--- src/Materi/Styling/inde.jsx
+++ src/Materi/Styling/inde.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Plain from "./Plain";
-
+import Sass from "./Sass";
 
 export default class Styling extends React.Component {
+    
     render() {
         return (
             <div>
-                <Plain />
+                <Sass /> 
             </div>
         )
     }
