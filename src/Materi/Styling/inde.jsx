10fbaefa7b91c394c2e98bd1c6697cb40d9b1305
lestariayu99 <lestariayu0801@gmail.com>
2023-06-11 04:18:40 +0700

    modifikasi file json

--- src/Materi/Styling/inde.jsx
+++ src/Materi/Styling/inde.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Styled from "./Styled";
+import Bootstrap from "./Bootstrap";
 
 export default class Styling extends React.Component {
     
     render() {
         return (
             <div>
-                <Styled />
+               <Bootstrap />
             </div>
         )
     }
