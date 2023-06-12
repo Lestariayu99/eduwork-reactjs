c83bb993c6db222176e5f19b1f1c588b42c5f587
lestariayu99 <lestariayu0801@gmail.com>
2023-06-13 01:55:34 +0700

    memindahkan file img.jpg

--- src/Materi/Styling/Bootstrap/index.jsx
+++ src/Materi/Styling/Bootstrap/index.jsx
@@ -1,14 +1,39 @@
-import App from './App';
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from 'react';
+import './style.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-      <Profile />
-    </div>
+    <Container>
+      <header>
+        <h5>Portfolio</h5>
+      </header>
+
+      <main>
+        <div className="profile">
+          <img src="img.jpg" alt="Profile Picture" />
+          <h5>Ayu Lestari</h5>
+        </div>
+
+        <div className="links">
+          <ul>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M9 19V5l7 7-7 7z" />
+              </svg>
+              <a href="https://www.facebook.com/tarilesta88/">Facebook</a>
+            </li>
+            {/* ... */}
+          </ul>
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          <h6>&copy; 2023 My Portfolio. Everything gonna be okay.</h6>
+        </p>
+      </footer>
+    </Container>
   );
 }
 
