534d3ed61600610b672bb1dde1d21f8274d5bc90
lestariayu99 <lestariayu0801@gmail.com>
2023-06-13 22:02:00 +0700

    mengubah index.js

--- src/Materi/Styling/Bootstrap/index.jsx
+++ src/Materi/Styling/Bootstrap/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import './style.css';
 import { Container } from 'react-bootstrap';
+import './style.css';
+
 
 function App() {
   return (
@@ -10,28 +11,73 @@ function App() {
       </header>
 
       <main>
-        <div className="profile">
-          <img src="img.jpg" alt="Profile Picture" />
-          <h5>Ayu Lestari</h5>
-        </div>
+        <section className="profile">
+          <img src="photo.png" alt="" /> 
+          <h5 style={{ fontSize: '0.8em' }}>Ayu Lestari</h5>
 
-        <div className="links">
+        </section>
+
+        <section className="links">
           <ul>
             <li>
-              <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
                 <path d="M9 19V5l7 7-7 7z" />
               </svg>
               <a href="https://www.facebook.com/tarilesta88/">Facebook</a>
             </li>
-            {/* ... */}
+
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 2H3a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-8 11v4m0 0V9m0 4H9m6 0h2" />
+              </svg>
+              <a href="https://www.instagram.com/ayule99/">Instagram</a>
+            </li>
+
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M22.5 7.5l-4 4L12 21l-5.5-5.5-4-4L2 9.5 12 3l10 6.5z" />
+              </svg>
+              <a href="https://twitter.com/ayulestariwandi">Twitter</a>
+            </li>
           </ul>
-        </div>
+        </section>
       </main>
 
       <footer>
-        <p>
-          <h6>&copy; 2023 My Portfolio. Everything gonna be okay.</h6>
-        </p>
+        <p>&copy; 2023 My Portfolio. Everything gonna be okay.</p>
       </footer>
     </Container>
   );
